51ca3bbca81afa7a4b8dd02d469296b435b318b5
Gabriel Rubinsztain <99496507+Gabriel-Rubinsztain@users.noreply.github.com>
2022-05-09 19:54:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
 from flask import Flask, render_template, json, request
-from flaskext.mysql import MySQL
+from model.product import Product
 
 app = Flask(__name__)
 
 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'mudar123'
-app.config['MYSQL_DB'] = 'teste'
-app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://ac03:123456@db:5432/products'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-mysql = MySQL(app)
+db.init_app(app)
 
 
 @app.route("/", methods=['GET'])
@@ -26,12 +24,14 @@ def register():
 
     try:
         if nome and categoria and preco:
+            db.create_all()
+            db.session.commit()
 
-            cursor = mysql.connection.cursor()
-            cursor.execute(
-                'INSERT INTO cadastro VALUES (% s, % s, % s)', (nome, categoria, preco))
-            mysql.connection.commit()
-            cursor.close()
+            product = Product(name=nome, category=categoria,
+                        price=preco)
+
+            db.session.add(product)
+            db.session.commit()
             return render_template('signup.html')
     except Exception as e:
         print(e)
@@ -40,10 +40,17 @@ def register():
 
 @app.route('/list')
 def list():
-    cursor = mysql.connection.cursor()
-    cursor.execute("select * from cadastro")
-    rows = cursor.fetchall()
-    return render_template("list.html", rows=rows)
+    all_products = []
+        products = Product.query.all()
+        for product in products:
+            result = {
+                "id": product.id,
+                "name": product.name,
+                "category": product.category,
+                "price": product.price,
+            }
+            all_products.append(result)
+    return render_template("list.html", rows=all_products)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5005))
