863bd9202626fde45425b0812487d17d6fd180b6
Gabriel Rubinsztain <99496507+Gabriel-Rubinsztain@users.noreply.github.com>
2022-05-09 21:35:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, json, request, jsonify
+from flask import Flask, render_template, json, request, jsonify, make_response
 from model.product import db
 from model.product import Product
 
